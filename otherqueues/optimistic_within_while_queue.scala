49ed62d7a38fe049ea4addcf115b4b9797259f08
Bruno Didot <bdidot@gmail.com>
2012-06-06 02:20:47 +0200

    Compared some implementations of enqueue

---
+++ otherqueues/optimistic_within_while_queue.scala
@@ -0,0 +1,280 @@
+/* enqueue, 1 thread, 40'000'000 elements :
+ * 557 526 599 600 599 599 600 599 600 599 601 599 599 601 600 600 599 599 600 599 600 600 600 599 600 599 600 599 599 600 600 599 605 599 600 600 600 599 600 600
+ *
+ * enqueue, 4 threads, 4'000'000 elements / thread :
+ * 1012 986 1013 1032 977 977 979 981 972 980 984 980 979 970 972 978 985 978 986 977 988 979 982 979 981 978 979 975 978 977 986 979 981 974 976 980 983 981 972 982
+ * 
+ * enqueue, 8 threads, 4'000'000 elements / thread :
+ * 2059 2079 2092 2021 2145 2137 2024 2011 2134 2010 2134 2000 2127 2005 2005 2131 2122 2020 2129 2133 2022 2013 2131 2114 2128 2127 2132 2125 2135 2139 2130 2135 2140 2125 2133 2129 2125 2126 2126 2132
+ */
+
+package scala.concurrent
+
+import java.util.concurrent.atomic._
+
+class ConcurrentUnrolledQueue[A] {
+//  override def companion: scala.collection.generic.GenericCompanion[ConcurrentUnrolledQueue] = ConcurrentUnrolledQueue
+
+  import ConcurrentUnrolledQueue._
+
+  def enqueue(elem: A): Unit = {
+    if (elem == null) {
+      throw new NullPointerException("Queue cannot hold null values.")
+    }
+
+    val NODE_SIZE = Node.NODE_SIZE
+
+    while (true) {
+      val t = tail
+      var i = t.addHint
+
+      if (i < NODE_SIZE) {
+        if (t.compareAndSwapElem(i, null, elem)) {
+          t.addHint = i + 1
+          return
+        }
+      }
+
+      val n = t.next.get
+      if (n == null) {
+        i = t.addHint
+        while (i < NODE_SIZE && t.get(i) != null) {
+          i += 1
+        }
+
+        if (i < NODE_SIZE) {
+          if (t.compareAndSwapElem(i, null, elem)) {
+            t.addHint = i + 1
+            return
+          } // else: could not insert elem in node, try again
+        } else { // if (i == Node.NODE_SIZE)
+          val n_ = new Node(elem)
+          if (t.compareAndSwapNext(null, n_)) {
+            compareAndSwapTail(t, n_)
+            return
+          } // else: could not add Node at end of queue, try again
+        }
+      } else { // tail does not point to end of list, try to advance it, then try again
+        compareAndSwapTail(t, n)
+      }
+    }
+  }
+
+  def dequeue(): A = {
+    while (true) {
+      val h = head
+      val nh = h.next.get
+      val t = tail
+
+      if (h == t) {
+        if (nh == null) {
+          return null.asInstanceOf[A]
+        }
+        compareAndSwapTail(t, nh) // Tail is falling behind.  Try to advance it
+      } else {
+        var i = nh.deleteHint
+        var v : Any = null
+
+        while (i < Node.NODE_SIZE && { v = nh.get(i); v == DELETED }) {
+          i += 1
+        }
+
+        if (v == null) {
+          /* this needs some more thinking. And an assert that would look like assert(nh == t,...) but it would'nt be thread safe */
+          return null.asInstanceOf[A]
+        }
+
+        if (i < Node.NODE_SIZE_MIN_ONE) {
+          if (nh.compareAndSwapElem(i, v, DELETED)) {
+            nh.deleteHint = i
+            return v.asInstanceOf[A]
+          }
+        } else if (i == Node.NODE_SIZE_MIN_ONE) { // if the element being removed is the last element of the node...
+          /* This needs some more careful thinking. */
+          /* 23.05.2012, pretty sure about this one now */
+          if (compareAndSwapHead(h, nh)) {
+            nh.set(Node.NODE_SIZE_MIN_ONE, DELETED)
+            return v.asInstanceOf[A]
+          }
+        } else { // if (i == Node.NODE_SIZE)
+          // All the elements of this node have been deleted : node has been deleted.
+        }
+      }
+    }
+
+    return null.asInstanceOf[A] // should never happen, maybe throw an exception instead ?
+  }
+
+  def peek(): A = {
+    while (true) {
+      val h = head
+      val nh = h.next.get
+      val t = tail
+
+      if (h == t) {
+        if (nh == null) {
+          return null.asInstanceOf[A]
+        }
+        compareAndSwapTail(t, nh) // Tail is falling behind.  Try to advance it
+      } else {
+        var i = nh.deleteHint
+        var v : Any = null
+
+        while (i < Node.NODE_SIZE && { v = nh.get(i); v == DELETED }) {
+          i += 1
+        }
+
+        /* it would be nice to advance deleteHint */
+
+        if (i < Node.NODE_SIZE) {
+          return v.asInstanceOf[A]
+        } else {
+          /* node has been deleted. */
+        }
+      }
+    }
+
+    return null.asInstanceOf[A]
+  }
+
+  /* a simple size implementation. not very useful in a concurrent context */
+  def size(): Int = {
+    var count = 0
+    var current = head
+    while ({ current = current.next.get; current != null }) {
+      var i = current.deleteHint
+      while (i < Node.NODE_SIZE) {
+        val elem = current.get(i)
+        if (elem == null) {
+          return count
+        } else if (elem != DELETED) {
+          count += 1
+        }
+      }
+    }
+    count
+  }
+
+//  @scala.inline
+  private def compareAndSwapHead(expect: Node, update: Node) = {
+    UNSAFE.compareAndSwapObject(this, HEAD_OFFSET, expect, update)
+  }
+
+//  @scala.inline
+  private def compareAndSwapTail(expect: Node, update: Node) = {
+    UNSAFE.compareAndSwapObject(this, TAIL_OFFSET, expect, update)
+//    tail.compareAndSet(expect, update)
+  }
+
+  private val DELETED = new AnyRef()
+
+  @volatile
+  private var head = new Node()
+
+  @volatile
+  private var tail = head
+
+  {
+    var i = 0
+    while (i < Node.NODE_SIZE) {
+      head.set(i, DELETED)
+      i += 1
+    }
+  }
+
+  final class Node () {
+    import Node._
+
+    /**
+     * Creates a node with an initial element and sets the hint to 1
+     */
+    def this(firstElem: Any) = {
+      this()
+      set(0, firstElem)
+      addHint = 1
+    }
+
+//    @scala.inline
+    def compareAndSwapElem(i: Int, expect: Any, update: Any) = {
+//      UNSAFE.compareAndSwapObject(elements, ELEMENTS_OFFSET + i * ELEMENTS_INDEX_STEP, expect, update)
+      elements.compareAndSet(i, expect, update)
+    }
+
+//    @scala.inline
+    def compareAndSwapNext(expect: Node, update: Node) = {
+//      UNSAFE.compareAndSwapObject(this, NEXT_OFFSET, expect, update)
+      next.compareAndSet(expect, update)
+    }
+
+//    @scala.inline
+    def set(i : Int, elem : Any) = {
+//      UNSAFE.putObjectVolatile(elements, ELEMENTS_OFFSET + i * ELEMENTS_INDEX_STEP, elem)
+      elements.set(i, elem)
+    }
+
+//    @scala.inline
+    def get(i : Int) = {
+//      UNSAFE.getObjectVolatile(elements, ELEMENTS_OFFSET + i * ELEMENTS_INDEX_STEP)
+      elements.get(i)
+    }
+
+//    def mkString(): String = {
+//      var i = 0
+//      var s = ""
+//      while (i < atomicElements.length()) {
+//        s += atomicElements.get(i)
+//        i += 1
+//      }
+//      return s
+//    }
+
+//    val elements = new Array[Any](NODE_SIZE)
+    val elements = new AtomicReferenceArray[Any](NODE_SIZE)
+
+    @volatile
+    var next = new AtomicReference[Node](null)
+
+    @volatile
+    var addHint = 0
+
+    @volatile
+    var deleteHint = 0
+  }
+
+  object Node {
+
+    val NODE_SIZE = 1 << 12
+
+    val NODE_SIZE_MIN_ONE = NODE_SIZE - 1
+
+//    val NEXT_OFFSET = UNSAFE.objectFieldOffset(classOf[Node].getDeclaredField("next"))
+
+//    val ELEMENTS_OFFSET = UNSAFE.arrayBaseOffset(classOf[Array[Any]])
+
+//    val ELEMENTS_INDEX_STEP = UNSAFE.arrayIndexScale(classOf[Array[Any]])
+
+  }
+
+}
+
+object ConcurrentUnrolledQueue {
+
+  val UNSAFE = {
+    if (this.getClass.getClassLoader == null)
+      sun.misc.Unsafe.getUnsafe()
+    else
+      try {
+        val fld = classOf[sun.misc.Unsafe].getDeclaredField("theUnsafe")
+        fld.setAccessible(true)
+        fld.get(null).asInstanceOf[sun.misc.Unsafe]
+      } catch {
+        case e => throw new RuntimeException("Could not obtain access to sun.misc.Unsafe", e)
+      }
+  }
+
+  val HEAD_OFFSET = UNSAFE.objectFieldOffset(classOf[ConcurrentUnrolledQueue[_]].getDeclaredField("head"))
+
+  val TAIL_OFFSET = UNSAFE.objectFieldOffset(classOf[ConcurrentUnrolledQueue[_]].getDeclaredField("tail"))
+
+}
+
